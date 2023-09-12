446fcb4c1d00806695848a6a162cd2df6f5655f6
wupeng <>
2023-09-12 18:12:13 +0800

    修改restart.sh脚本并新增start.sh脚本

--- restart.sh
+++ restart.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "/data/open-c3-federation/data" ]]; then
-    echo "错误: /data/open-c3-federation/data 目录不存在!"
-    exit 1
-fi
-
-if [[ ! -f "/data/open-c3-federation/data/.env" ]]; then
-    echo "错误: /data/open-c3-federation/data/.env 文件不存在!"
-    exit 1
-fi
-
-if [[ ! -f "/data/open-c3-federation/data/config.json" ]]; then
-    echo "错误: /data/open-c3-federation/data/config.json 文件不存在!"
-    exit 1
-fi
-
 bash ./stop.sh
-
-PORT=$(python -c 'import json; print(json.load(open("config.json"))["port"])')
-
-docker run --restart=always -v /data/open-c3-federation/data/.env:/app/.env \
-  -v /data/open-c3-federation/data/config.json:/app/config.json \
-  -v /data/open-c3-federation/data/approve_id.txt:/app/data/approve_id.txt \
-  -p $PORT:$PORT -d --name open-c3-federation open-c3-federation:latest
+bash ./start.sh
