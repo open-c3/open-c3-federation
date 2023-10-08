b98fffa9ecbb0e4a9e8d62390936fbe9164e0b5c
guozixin@cmcm.com <guozixin@cmcm.com>
2023-10-08 19:37:50 +0800

    style(login): 修改登录C3按钮样式

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Image } from 'antd';
+import { Image, Button } from 'antd';
 
 import { parseQueryParams } from '@/utils/index';
 
@@ -12,12 +12,21 @@ import style from './index.module.less';
 
 const LoginPage: React.FC = () => {
     const [platformList, setPlatformList] = useState<ILoginPlatform[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [loadingBox] = useState<Array<string>>(
+        [...Array(5).keys()].map((item: number) => `rect${item + 1}`.toString())
+    );
+
     useEffect(() => {
         const getData = async () => {
+            setLoading(true);
             const dataRet = await getEndpoints();
             if (dataRet) {
                 const { data } = dataRet;
+                setLoading(false);
                 setPlatformList(data || []);
+            } else {
+                setLoading(false);
             }
         };
         getData();
@@ -39,14 +48,27 @@ const LoginPage: React.FC = () => {
                 <Image width={234} src={c3Logo} preview={false} />
             </div>
             <div className={style['login-address']}>
-                <div style={{ marginTop: '67px' }}>请选择要跳转的平台</div>
-                <div className={style['platform-list']}>
-                    {platformList.map((item: any) => (
-                        <div key={item.name} className={style['li-items']} style={{ margin: '20px 0' }}>
-                            <div onClick={() => handleJump(item.name)}>登录{item.name}平台</div>
-                        </div>
-                    ))}
-                </div>
+                <div>请选择要跳转的平台</div>
+                {loading ? (
+                    <div className={style['translate-content']}>
+                        {loadingBox.map((item: string) => {
+                            return <div key={item} className={style[item]}></div>;
+                        })}
+                    </div>
+                ) : (
+                    <div className={style['platform-list']}>
+                        {platformList.map((item: any) => (
+                            <Button
+                                disabled={item.login === 0}
+                                key={item.name}
+                                className={style['li-items']}
+                                onClick={() => handleJump(item.name)}
+                            >
+                                登录{item.name}平台
+                            </Button>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
